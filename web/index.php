7918429f339293ea8fd0dea6587dc11af571de5d
Sam Heuck <sam@samheuck.com>
2014-07-22 01:42:00 -0500

    adds twig
    Signed-off-by: Sam Heuck <sam@samheuck.com>

--- web/index.php
+++ web/index.php
@@ -3,9 +3,14 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
-$app->get('/hello', function (Silex\Application $app) {
-    return "Hello World!";
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/../views',
+));
+
+$app->get('/home', function (Silex\Application $app) {
+    return $app['twig']->render('index.twig');
 });
 
 $app->run();
